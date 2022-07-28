55b048bbb72534f7106d3154cbe34ed40f0c6fd8
ylose <ylose@192.168.123.105>
2022-07-28 12:44:01 +0900

    비즈니스 클래스 작성

---
+++ SampleWeb/src/main/java/egovframework/sample/service/impl/SampleServiceImpl.java
@@ -0,0 +1,28 @@
+package egovframework.sample.service.impl;
+
+public class SampleServiceImpl {
+	
+	public SampleServiceImpl() throws Exception {
+		System.out.println("===> SampleServiceImpl 생성");
+	}
+	
+	public void insertSample() throws Exception {
+		System.out.println("SampleService---Sample 등록");
+	}
+	
+	public void updateSample() throws Exception {
+		System.out.println("SampleService---Sample 수정");
+	}
+	
+	public void deleteSample() throws Exception {
+		System.out.println("SampleService---Sample 삭제");
+	}
+	
+	public void selectSample() throws Exception {
+		System.out.println("SampleService---Sample 상세 조회");
+	}
+	
+	public void selectSampleList() throws Exception {
+		System.out.println("SampleService---Sample 목록 검색");
+	}
+}
