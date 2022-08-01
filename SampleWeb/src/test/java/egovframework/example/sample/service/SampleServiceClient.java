64528021aa9be2e33a41f5160b83290ac24b78f2
ylose <ylose@192.168.123.105>
2022-08-01 15:34:54 +0900

    객체를 getBean메소드로 검색한 후 그 객체의 메소드들을 호출한 후 컨테이너를 종료하는 소스 추가

--- SampleWeb/src/test/java/egovframework/example/sample/service/SampleServiceClient.java
+++ SampleWeb/src/test/java/egovframework/example/sample/service/SampleServiceClient.java
@@ -3,11 +3,20 @@ package egovframework.example.sample.service;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import egovframework.sample.service.impl.SampleServiceImpl;
+
 public class SampleServiceClient {
 	public static void main(String[] args) throws Exception {
 		// 1. Spring 컨테이너를 구동한다.
 		AbstractApplicationContext container = new GenericXmlApplicationContext(
 				"egovframework/spring/context-common.xml");
-				
+		
+		// 2. Spring 컨테이너로부터 SampleServiceImpl 객체를 Lookup한다.
+		SampleServiceImpl sampleService = (SampleServiceImpl) container.getBean("sampleService");
+		sampleService.insertSample();
+		sampleService.selectSampleList();
+		
+		// 3. Spring 컨테이너를 종료한다.
+		container.close();
 	}
 }
